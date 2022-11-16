983fb9b74a2f426cc0c460efa423c45efd5a5168
Araam Zaremehrjardi <azaremehrjardi@unr.edu>
2022-11-16 00:01:56 -0800

    Developed update_config for /device/config route

--- backend/blueprints/group_blueprint.py
+++ backend/blueprints/group_blueprint.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 group_blueprint = Blueprint(name="group_blueprint", import_name=__name__)
 
 @group_blueprint.route("/config/<uuid>")
 def update_config(uuid):
     
+    configuration = request.get_json()
+    
+
+
     return jsonify({"config":"group " + uuid})
 
 @group_blueprint.route("/status/<uuid>")
--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,14 @@ from blueprints.device_blueprint import device_blueprint
 from blueprints.group_blueprint import group_blueprint
 
 api = Flask(__name__)
-mqtt = Mqtt(app=api)
-api.config['MQTT_BROKER_URL'] = None
-api.config['MQTT_BROKER_PORT'] = 0
-api.config['MQTT_USERNAME'] = ''
-api.config['MQTT_PASSWORD'] = ''
-api.config['MQTT_KEEPALIVE'] = ''
-api.config['MQTT_TLS_ENABLED'] = False
+#mqtt = Mqtt(app=api)
+
+#api.config['MQTT_BROKER_URL'] = None
+#api.config['MQTT_BROKER_PORT'] = 0
+#api.config['MQTT_USERNAME'] = ''
+#api.config['MQTT_PASSWORD'] = ''
+#api.config['MQTT_KEEPALIVE'] = ''
+#api.config['MQTT_TLS_ENABLED'] = False
 
 api.register_blueprint(device_blueprint, url_prefix="/device")
 api.register_blueprint(group_blueprint, url_prefix="/group")
