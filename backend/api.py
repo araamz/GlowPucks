c6206e0808f992a6f525ca9791e5c1b582fc43e4
Araam Zaremehrjardi <azaremehrjardi@unr.edu>
2022-11-24 03:18:01 -0800

    Finished Backend

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_mqtt import Mqtt
 
-from .device import construct_configuration
+import json
+
+from .device import construct_device_configuration, construct_device_status
+from .group import construct_group_configuration
 
 api = Flask(__name__)
 api.config['MQTT_BROKER_URL'] =  '10.42.0.1'
@@ -21,7 +24,7 @@ def process_device_configuration(uuid):
     group_enable = configuration_request['group_enable']
     group_target = configuration_request['group_target']
 
-    configuration = construct_configuration(
+    configuration = construct_device_configuration(
         label = label,
         color = color,
         brightness = brightness,
@@ -50,12 +53,53 @@ def process_device_status(uuid):
     
     @mqtt.on_topic(topic)
     def view_status(client, userdata, message):
-        status = message.payload.decode()
+        status = json.loads(message.payload.decode())
 
     mqtt.unsubscribe(topic)
 
-    print(status)
+    status_response = construct_device_status(status)
 
+    return jsonify(status_response)
+
+@api.route('group/config/<uuid>')
+def process_group_configuration(uuid):
+
+    configuration_request = request.get_json()
+    color = configuration_request['color']
+    brightness = configuration_request['brightness']
+    active = configuration_request['active']
+    mode = configuration_request['mode']
+    device_targets = configuration_request['targets']
+
+    mqtt_successful_response = list()
+
+    for target in device_targets:
+
+        topic = f'/device/config/{target}'
+
+        device_configuration = construct_group_configuration(
+            brightness = brightness,
+            active = active,
+            color = color,
+            mode = mode,
+            group_enable = True,
+            group_target = uuid
+        )
+
+        successful =  mqtt.publish(
+            topic = topic,
+            payload = device_configuration
+        )[0]
+
+        mqtt_successful_response.append(successful) 
+
+
+
+    # Create a for-loop that subscribes to a device '/device/config/<uuid>' topic
+    # Get the config, process it as a individual config - label, brightness, active, color, mode
+    # For each config transmission, add the group_enable, group_target parameters
+
+    return jsonify(mqtt_successful_response)
 '''
 mqtt.subscribe('/device/config/4e51c2ac-9d96-4f3c-932c-1069741285fd')
 @mqtt.on_topic('/device/config/4e51c2ac-9d96-4f3c-932c-1069741285fd')
--- backend/device.py
+++ backend/device.py
@@ -1,6 +1,6 @@
 import json
 
-def construct_configuration(label, brightness, active, color, mode, group_enable, group_target):
+def construct_device_configuration(label, brightness, active, color, mode, group_enable, group_target):
 
     mqtt_configuration = dict()
 
@@ -23,3 +23,56 @@ def construct_color(mode, color):
         return alternate_colors
 
     return color
+
+message_test = {
+    "label":"GP 1",
+    "brightness": 100,
+    "active": True,
+    "mode": "alternate",
+    "color": {
+        "primary_color": "red",
+        "secondary_color": "blue"
+    },
+    "group_enable": True,
+    "group_target": "4bef278d-eefb-44c5-be0d-99a6eae2b5ba"
+}
+
+def construct_device_status(mqtt_status):
+
+    label = mqtt_status['label']
+    brightness = mqtt_status['brightness']
+    active = mqtt_status['active']
+    mode = mqtt_status['mode']
+    color = construct_color(mode, mqtt_status['color'])
+    group_enable = mqtt_status['group_enable']
+    group_target = mqtt_status['group_target']
+    car_count = mqtt_status['car_count']
+
+    device_status_response = None
+
+    if (group_enable):
+
+        device_status_response = {
+            "label": label,
+            "brightness": brightness,
+            "active": active, 
+            "mode": mode,
+            "color": color,
+            "group_enable": group_enable,
+            "group_target": group_target,
+            "car_count": car_count
+        }
+
+    else:
+
+        device_status_response = {
+            "label": label,
+            "brightness": brightness,
+            "active": active, 
+            "mode": mode,
+            "color": color,
+            "group_enable": group_enable,
+            "car_count": car_count
+        }
+
+    return device_status_response
